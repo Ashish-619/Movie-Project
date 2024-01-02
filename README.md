81cc9f5330417f746e4234de00c9ae2007282539
Ashish-619 <ashishvaidya2000@gmail.com>
2024-01-02 16:38:14 +0530

    commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Welcome to the Movie App - Your go-to destination for exploring all your favorite films! Discover star-studded casts, captivating trailers, insightful reviews, and more, all in one place
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import "./style.scss";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Explore the world of cinema with Movix! Your go-to destination for
+          discovering the latest releases, exploring timeless classics, and
+          diving into the fascinating universe of movies. From gripping dramas
+          to heart-pounding thrillers, Movix is your cinematic companion. Stay
+          entertained, stay informed, and immerse yourself in the magic of
+          storytelling. Lights, camera, action – it's all at your fingertips
+          with Movix!
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
+};
+
+export default Footer;
